d022ee7c0e3580b52ab50ba6c68cc6ad190b1872
KozlovOleg97 <kozlovoleg2021@gmail.com>
2022-10-26 23:52:22 +0300

    Add project files.

---
+++ Debt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainee_Project
+{
+    internal class Debt
+    {
+        private double _Balance { get; set; }
+        private double _InterestRate { get; set; }
+
+        public Debt(double initialBalance, double initialInterestRate)
+        {
+            _Balance = initialBalance;
+            _InterestRate = initialInterestRate;
+        }
+
+        public void HoldOnOneYear()
+        {
+            _Balance = _Balance * _InterestRate;
+            
+        }
+
+        public void ShowBalance()
+        {
+            Console.WriteLine($"_Balance:{_Balance} InterestRate{_InterestRate}");
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Trainee_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Debt mortgage = new Debt(120000.0, 1.01);
+            mortgage.ShowBalance();
+
+            mortgage.HoldOnOneYear();
+            mortgage.ShowBalance();
+
+            // Wait 20 years
+            int years = 0;
+            while (years < 20)
+            {
+                mortgage.HoldOnOneYear();
+                years = years + 1;
+            }
+            mortgage.ShowBalance();
+        }
+    }
+}
